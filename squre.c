cf2269c8804a521f970b330161199c0f01172ffa
rohit8002564 <103364577+rohit8002564@users.noreply.github.com>
2022-05-23 09:13:29 +0530

    rp

---
+++ squre.c
@@ -0,0 +1,17 @@
+ //Area of squre
+    #include <stdio.h>
+    struct square
+    {
+        int lenth;
+        int breadth;
+    }a,b;
+    int main()
+    {
+
+        a.lenth = 5;       
+       b.breadth = 5;
+        printf ("Area of square : %d",a.lenth*b.breadth);
+        return 0;
+
+
+    }
